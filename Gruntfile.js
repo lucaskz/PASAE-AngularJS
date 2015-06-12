e9093f89b48aa46a9aab099bc39c32e2fdfcb0e5
lkaseta <lkaseta@cm0542.cm.fedpat.com.ar>
2015-06-12 10:42:31 -0300

    ip

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
       options: {
         port: 8080,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: '0.0.0.0',
+        hostname: '186.137.51.226',
         livereload: 35729
       },
       livereload: {
