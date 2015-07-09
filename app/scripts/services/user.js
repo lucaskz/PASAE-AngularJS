98c7dbb4f51bf374eead3144ce608d4d1c8ad694
EzequielPanoff <collectorsw2004@yahoo.com.ar>
2015-07-09 02:11:23 -0300

    Developing views

--- app/scripts/services/user.js
+++ app/scripts/services/user.js
@@ -7,7 +7,7 @@ angular.module('pasaeAngularJsApp').service( 'UsuarioService', ['$q','$http','$h
 //			$http.post('http://localhost:8080/web-module/espectador', user).then(function(successData){
 //				var data = successData;
 //				// se registra exitosamente el usuario devuelvo la informacion para logearlo
-//				deferred.resolve(data);				
+//				deferred.resolve(data);
 //				},function(error){
 //					deferred.reject(error);
 //				});
@@ -15,16 +15,16 @@ angular.module('pasaeAngularJsApp').service( 'UsuarioService', ['$q','$http','$h
 //		},
 		getUserData : function(){
 			var deferred = $q.defer();
-			
-			$http.get('http://localhost:8080/web-module/espectador/1').then(function(successData){
+
+			$http.get('http://localhost:8080/web-module/espectador/0').then(function(successData){
 				var data = successData;
-				
-				deferred.resolve(data);				
+
+				deferred.resolve(data);
 				},function(error){
 					deferred.reject(error);
 				});
 			return deferred.promise;
 		}
-	
+
 	};
 }]);
