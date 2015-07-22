5fac525940f090ad8436523d74aadbf56ed69cf6
EzequielPanoff <collectorsw2004@yahoo.com.ar>
2015-07-21 21:44:27 -0300

    Desarrollando ABM (usuarioABM...)

--- app/scripts/services/usuario.js
+++ app/scripts/services/usuario.js
@@ -23,6 +23,19 @@ angular.module('pasaeAngularJsApp').service( 'UsuarioService', ['$q','$http','$h
                 }
 
                 return deferred.promise;
+          },
+
+          editarEspectaculo:function() {
+
+                 var deferred = $q.defer();
+
+                 $http.get('http://localhost:8080/web-module/usuario/0').then(function(successData){
+                    var data = successData;
+                   	deferred.resolve(data);
+                 },function(error){
+                    deferred.reject(error);
+                 });
+                 return deferred.promise;
           }
   }
 }]);
