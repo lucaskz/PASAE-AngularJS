804ffbcaee41919f072d21705a019bd2cc2953b9
EzequielPanoff <collectorsw2004@yahoo.com.ar>
2015-08-09 21:41:50 -0300

    Completado imagen...

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function(config) {
       'bower_components/ngstorage/ngStorage.js',
       'bower_components/angular-utils-pagination/dirPagination.js',
       'bower_components/ng-file-upload/ng-file-upload.js',
+      'bower_components/angular-wurfl-image-tailor/src/angular-wurfl-image-tailor.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
